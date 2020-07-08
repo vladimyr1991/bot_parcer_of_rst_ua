4821d0afc7560cfa8663d431bc19aa68f8f63f79
Vladimir Moroz <vmoroz@medicata.ru>
2020-07-08 11:53:51 +0300

    писание README.md

--- bot.py
+++ bot.py
@@ -1,36 +1,73 @@
 # -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+from fake_useragent import UserAgent
+import time
+import sqlite3
+import telebot
+from telebot.types import Message
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import apiai, json
+TOKEN = '1315762320:AAGrFbOXp4w427CeGY_ofPxCyCR5-Uu5gf0'
+STICKER = 'CAACAgIAAxkBAAMWXvzXF0uv5uC6PiR6baq00_jS7L8AAk4AA7aPSgnHCXNG_QI0jBoE'
 
-updater = Updater(token='991640720:AAGf3na0QDBQUUlbeYb5HRyA-38I1xebtqE') 
-dispatcher = updater.dispatcher
+bot = telebot.TeleBot(TOKEN)
+
+# создаем таблицу если отсустсвует
+try:
+    cursor.execute("""CREATE TABLE list_of_refs
+                    (href text)
+                   """)
+except:
+    pass
 
 
+web_site = 'https://rst.ua'
 
-def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+regions = {'komsomolsk':'http://rst.ua/oldcars/poltava/499',
+           'kremenchug':'http://rst.ua/oldcars/poltava/501',
+           'svetlovodsk':'http://rst.ua/oldcars/kirovograd/305'}
 
-def textMessage(bot, update):
-    request = apiai.ApiAI('3c8ba04b3b2c471385186b7ee511e950').text_request() # Токен API к Dialogflow
-    request.lang = 'ru' # На каком языке будет послан запрос
-    request.session_id = 'talker' # ID Сессии диалога (нужно, чтобы потом учить бота)
-    request.query = update.message.text # Посылаем запрос к ИИ с сообщением от юзера    
-    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
-    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
-    if response:
-        bot.send_message(chat_id=update.message.chat_id, text=response)
-    else:
-        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+conn = sqlite3.connect("list_of_cars.db")
+cursor = conn.cursor()
+cursor.execute('''SELECT * FROM list_of_refs''')
+row = cursor.fetchall()
+data_base = [x[0] for x in row]
 
-# Хендлеры
-start_command_handler = CommandHandler('start', startCommand)
-text_message_handler = MessageHandler(Filters.text, textMessage)
-# Добавляем хендлеры в диспетчер
-dispatcher.add_handler(start_command_handler)
-dispatcher.add_handler(text_message_handler)
-# Начинаем поиск обновлений
-updater.start_polling(clean=True)
-# Останавливаем бота, если были нажаты Ctrl + C
-updater.idle()
+def command_handler():
+    i  = True
+    while i:      
+        with sqlite3.connect("list_of_cars.db") as conn:
+            for city, link in regions.items():
+                for i in range(1,11):
+                    time.sleep(10)
+                    print(f'{city}:{i}')
+                    r = requests.get(f'{regions[city]}/{i}.html', headers={'User-Agent':UserAgent().chrome})
+                    html = r.content
+                    soup = BeautifulSoup(html,'html.parser')
+                    obj = soup.find_all('a', attrs = {'class':'rst-ocb-i-a'})
+                    obj_price = soup.find_all('span', attrs = {'class':'rst-uix-grey'})
+                    print(obj)
+                    for x in range(len(obj)):
+                        
+                        try:
+                            car_name = obj[x].find('span').text
+                            car_price = obj_price[x].text
+                        except (AttributeError,IndexError):
+                            car_name = obj[x]
+                            # car_price = obj_price[x].text
+                            
+                        link_to_car = obj[x].attrs['href']       
+                        if link_to_car not in data_base:
+                            cursor = conn.cursor()
+                            cursor.execute("""INSERT INTO list_of_refs VALUES (?)""",(link_to_car,))
+                            # Сохраняем изменения
+                            conn.commit()
+
+                            data_base.append(link_to_car)
+                            bot.send_message(624450338, f'{web_site}{link_to_car}')
+                            bot.send_message(624450338, f'ЦЕНА: {car_price}')
+                            
+                            bot.send_message(431200271, f'{web_site}{link_to_car}')
+                            bot.send_message(431200271, f'ЦЕНА: {car_price}')
+                            
+command_handler()
