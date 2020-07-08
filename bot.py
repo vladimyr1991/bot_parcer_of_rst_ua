899cb350be56ea346707c41d73f9054857b3a6c9
Vladimir Moroz <vmoroz@medicata.ru>
2020-07-08 21:53:14 +0300

    make it better

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ import sqlite3
 import telebot
 from telebot.types import Message
 import random
+from datetime import datetime
 
+print(datetime.now().strftime('%H'))
 
 
 TOKEN = '1315762320:AAGrFbOXp4w427CeGY_ofPxCyCR5-Uu5gf0'
@@ -44,48 +46,49 @@ def command_handler():
     
     while i:      
         with sqlite3.connect("list_of_cars.db") as conn:
-            for city, link in regions.items():
-                for i in range(1,11):
-                    
-                    print(f'{city}:{i}')
-                    if i == 1:
-                        r = requests.get(f'{regions[city]}', headers={'User-Agent':UserAgent().chrome})
-                    else:
-                        r = requests.get(f'{regions[city]}/{i}.html', headers={'User-Agent':UserAgent().chrome})
-                    if int(r.status_code) == 403:
-                        bot.send_message(431200271, f'Забанили')
-                        bot.send_message(624450338, f'Забанили')
+            if (int(datetime.now().strftime('%H'))>=6) and (int(datetime.now().strftime('%H'))<=22) :
+                for city, link in regions.items():
+                    for i in range(1,11):
                         
-                    html = r.content
-                    soup = BeautifulSoup(html,'html.parser')
-                    obj = soup.find_all('a', attrs = {'class':'rst-ocb-i-a'})
-                    obj_price = soup.find_all('span', attrs = {'class':'rst-uix-grey'})
-                    
-                    for x in range(len(obj)):
+                        print(f'{city}:{i}')
+                        if i == 1:
+                            r = requests.get(f'{regions[city]}', headers={'User-Agent':UserAgent().chrome})
+                        else:
+                            r = requests.get(f'{regions[city]}/{i}.html', headers={'User-Agent':UserAgent().chrome})
+                        if int(r.status_code) == 403:
+                            bot.send_message(431200271, f'Забанили')
+                            bot.send_message(624450338, f'Забанили')
+                            
+                        html = r.content
+                        soup = BeautifulSoup(html,'html.parser')
+                        obj = soup.find_all('a', attrs = {'class':'rst-ocb-i-a'})
+                        obj_price = soup.find_all('span', attrs = {'class':'rst-uix-grey'})
                         
-                        try:
-                            car_name = obj[x].find('span').text
-                            car_price = obj_price[x].text
-                        except (AttributeError,IndexError):
-                            car_name = obj[x]
-                            # car_price = obj_price[x].text
+                        for x in range(len(obj)):
                             
-                        link_to_car = obj[x].attrs['href']       
-                        if link_to_car not in data_base:
-                            cursor = conn.cursor()
-                            cursor.execute("""INSERT INTO list_of_refs VALUES (?)""",(link_to_car,))
-                            # Сохраняем изменения
-                            conn.commit()
+                            try:
+                                car_name = obj[x].find('span').text
+                                car_price = obj_price[x].text
+                            except (AttributeError,IndexError):
+                                car_name = obj[x]
+                                # car_price = obj_price[x].text
+                                
+                            link_to_car = obj[x].attrs['href']       
+                            if link_to_car not in data_base:
+                                cursor = conn.cursor()
+                                cursor.execute("""INSERT INTO list_of_refs VALUES (?)""",(link_to_car,))
+                                # Сохраняем изменения
+                                conn.commit()
 
-                            data_base.append(link_to_car)
-                            bot.send_message(624450338, f'{web_site}{link_to_car}')
-                            bot.send_message(624450338, f'ЦЕНА: {car_price}')
-                            
-                            bot.send_message(431200271, f'{web_site}{link_to_car}')
-                            bot.send_message(431200271, f'ЦЕНА: {car_price}')
-                    time.sleep(random.randint(10, 80))
-            time.sleep(random.randint(250, 350))            
-
+                                data_base.append(link_to_car)
+                                bot.send_message(624450338, f'{web_site}{link_to_car}')
+                                bot.send_message(624450338, f'ЦЕНА: {car_price}')
+                                
+                                bot.send_message(431200271, f'{web_site}{link_to_car}')
+                                bot.send_message(431200271, f'ЦЕНА: {car_price}')
+                        time.sleep(random.randint(10, 80))
+                time.sleep(random.randint(1245, 1650))            
+            time.sleep(3600)  
 if __name__ == "__main__":
-    # bot.polling(none_stop=True)
+
     command_handler()
